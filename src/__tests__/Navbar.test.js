2cea3ae1e6111e0eba68ce9a8a4f5f6d64983e49
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-01-26 14:31:12 +0500

    Add test for Navbar.js

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -1,1 +1,12 @@
 /* eslint-disable */
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from '../components/pages/Navbar';
+
+describe('test for Navbar component', () => {
+  it('should render Navbar component', () => {
+    expect(render(<Navbar />, { wrapper: BrowserRouter })).toMatchSnapshot();
+  });
+});
